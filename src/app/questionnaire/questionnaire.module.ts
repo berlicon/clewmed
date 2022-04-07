6ef4472c4c097a0bf7327225452db2c122752aa6
berlicon <berlicon@mail.ru>
2022-04-07 13:14:25 +0300

    add questionnaire module / component

---
+++ src/app/questionnaire/questionnaire.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+
+
+
+@NgModule({
+  declarations: [
+    QuestionnaireComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class QuestionnaireModule { }
