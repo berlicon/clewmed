94504d64854b57b040d5d1e4faf10b3020cc0aad
berlicon <berlicon@mail.ru>
2022-04-11 21:31:52 +0300

    remove useless tests

--- src/app/questionnaire/questionnaire-item/questionnaire-item.component.spec.ts
+++ src/app/questionnaire/questionnaire-item/questionnaire-item.component.spec.ts
@@ -18,8 +18,4 @@ describe('QuestionnaireItemComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
