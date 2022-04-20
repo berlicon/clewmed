725be21535b64a14ed993506fa435c2be1c66299
berlicon <berlicon@mail.ru>
2022-04-20 12:30:26 +0300

    add Material UI library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionnaireModule } from './questionnaire/questionnaire.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { QuestionnaireModule } from './questionnaire/questionnaire.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    QuestionnaireModule
+    QuestionnaireModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
